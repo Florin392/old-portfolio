141a3a48c56b6a6fdb682a4ff00d5136e780543e
florin392 <iordacheflorin3@yahoo.com>
2024-07-16 16:02:51 +0200

    add navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,11 +1,94 @@
-import { Grid, Typography } from "@mui/material";
+import { Divider, Grid, styled, Typography } from "@mui/material";
+// import "./NavigationBar.css";
 
 export default function NavigationBar() {
+  const HeaderContainer = styled(Grid)({
+    "@media screen and (max-width:899px)": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    "@media screen and (min-width:900px)": {
+      display: "flex",
+      flexDirection: "row-reverse",
+      position: "fixed",
+      rotate: "180deg",
+      writingMode: "vertical-rl",
+      justifyContent: "space-between",
+      alignItems: "center",
+      textAlign: "center",
+      width: "4%",
+      height: "90%",
+      paddingBottom: "10%",
+    },
+  });
+
+  const linkStyles = {
+    fontSize: "10px",
+    fontFamily: "Montserrat",
+    fontWeight: 500,
+    textTransform: "uppercase",
+    textDecoration: "none",
+    color: "#1a1818",
+    "&:hover": {
+      textDecoration: "line-through",
+    },
+  };
+
+  const StyledDivider = styled(Divider)({
+    marginRight: "7px",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1a1818",
+  });
+
   return (
-    <Grid>
-      <Typography>LinkedIn</Typography>
-      <Typography>Github</Typography>
-      <Typography variant="caption">©/2024</Typography>
-    </Grid>
+    <HeaderContainer>
+      <Grid
+        container
+        height="50%"
+        width={{ xs: "60%", md: "0" }}
+        direction={{ xs: "row", md: "row-reverse" }}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item xs={1}>
+          <Typography
+            component="a"
+            href="https://www.linkedin.com/in/florin-iordache-2b998b166/"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={linkStyles}
+          >
+            LinkedIn
+          </Typography>
+        </Grid>
+        <Grid item xs={1} py={2}>
+          <Typography
+            component="a"
+            href="https://github.com/Florin392"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={linkStyles}
+          >
+            Github
+          </Typography>
+        </Grid>
+        <Grid item xs={4} height={2}>
+          <StyledDivider orientation="vertical" />
+        </Grid>
+      </Grid>
+      <Grid container item xs={3} justifyContent={{ xs: "end", md: "start" }}>
+        <Typography
+          variant="caption"
+          sx={{
+            fontSize: "10px",
+            letterSpacing: "3px",
+          }}
+        >
+          ©2024
+        </Typography>
+      </Grid>
+    </HeaderContainer>
   );
 }
